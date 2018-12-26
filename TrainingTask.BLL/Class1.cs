0f62eff67a4f7fe991e202f72b709a7176a5efc7
Krasovskaya <KrasovskayaVS@qulix.com>
2018-12-26 17:06:32 +0300

    Add project files.

---
+++ TrainingTask.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TrainingTask.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ TrainingTask.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TrainingTask.Common
+{
+    public class Class1
+    {
+    }
+}
