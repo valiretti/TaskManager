5d032941dd4bc2d55cb9e7600098a620c43ae985
krasovskayavs <krasovskayavs@QWS-VKS-7>
2019-01-02 17:41:49 +0300

    + added logging implementation

---
+++ src/TrainingTask.Common/Interfaces/ILog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingTask.Common.Interfaces
+{
+    public interface ILog : IDisposable
+    {
+        /// <summary>
+        /// Log a message object with the Debug level.
+        /// </summary>
+        /// <param name="message">The message object to log.</param>
+        void Debug(string message);
+
+        /// <summary>
+        /// Log a message object with the Error level.
+        /// </summary>
+        /// <param name="message">The message object to log.</param>
+        void Error(string message);
+
+        /// <summary>
+        /// Log a message object with the Info level.
+        /// </summary>
+        /// <param name="message">The message object to log.</param>
+        void Info(string message);
+
+        /// <summary>
+        /// Log a message object with the Fatal level.
+        /// </summary>
+        /// <param name="message">The message object to log.</param>
+        void Fatal(string message);
+
+        /// <summary>
+        /// Log a message object with the Trace level.
+        /// </summary>
+        /// <param name="message">The message object to log.</param>
+        void Trace(string message);
+
+        /// <summary>
+        /// Log a message object with the Warn level.
+        /// </summary>
+        /// <param name="message">The message object to log.</param>
+        void Warn(string message);
+    }
+}
---
+++ src/TrainingTask.Common/Logging/Log.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TrainingTask.Common.Interfaces;
+
+namespace TrainingTask.Common.Logging
+{
+    public class Log : ILog
+    {
+        protected readonly object lockObj = new object();
+        private readonly StreamWriter _file;
+
+        public Log(string pathToLogging)
+        {
+            _file = new StreamWriter(pathToLogging, true);
+        }
+
+        public void Debug(string message)
+        {
+            lock (lockObj)
+            {
+                _file.Write($"{DateTime.Now} Debug: {message}");
+            }
+        }
+
+        public void Error(string message)
+        {
+            lock (lockObj)
+            {
+                _file.Write($"{DateTime.Now} Error: {message}");
+            }
+        }
+
+        public void Info(string message)
+        {
+            lock (lockObj)
+            {
+                _file.Write($"{DateTime.Now} Info: {message}");
+            }
+        }
+
+        public void Fatal(string message)
+        {
+            lock (lockObj)
+            {
+                _file.Write($"{DateTime.Now} Fatal: {message}");
+            }
+        }
+
+        public void Trace(string message)
+        {
+            lock (lockObj)
+            {
+                _file.Write($"{DateTime.Now} Trace: {message}");
+            }
+        }
+
+        public void Warn(string message)
+        {
+            lock (lockObj)
+            {
+                _file.Write($"{DateTime.Now} Warn: {message}");
+            }
+        }
+
+        public void Dispose()
+        {
+            lock (lockObj)
+            {
+                _file.Flush();
+                _file.Close();
+            }
+        }
+    }
+}
