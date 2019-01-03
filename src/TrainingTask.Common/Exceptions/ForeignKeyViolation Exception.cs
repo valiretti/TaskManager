931e8fdb478ec03f92f1be15b7ba68140537fad8
krasovskayavs <krasovskayavs@QWS-VKS-7>
2019-01-03 10:19:59 +0300

    + added custom error

---
+++ src/TrainingTask.Common/Exceptions/ForeignKeyViolation Exception.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace TrainingTask.Common.Exceptions
+{
+    public class ForeignKeyViolationException : Exception
+    {
+        public ForeignKeyViolationException()
+        {
+        }
+
+        public ForeignKeyViolationException(string message) : base(message)
+        {
+        }
+
+        public ForeignKeyViolationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ForeignKeyViolationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
