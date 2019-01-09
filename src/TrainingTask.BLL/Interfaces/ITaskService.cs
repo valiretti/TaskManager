f95f06152941dd54150ce24b8fe34d39ff168838
krasovskayavs <krasovskayavs@QWS-VKS-7>
2019-01-09 13:32:14 +0300

    changed services

--- src/TrainingTask.BLL/Interfaces/ITaskService.cs
+++ src/TrainingTask.BLL/Interfaces/ITaskService.cs
@@ -3,7 +3,7 @@ using TrainingTask.Common.Models;
 
 namespace TrainingTask.BLL.Interfaces
 {
-    public interface ITaskService
+    public interface ITaskService : IService<Task>
     {
         /// <summary>
         /// Adds the specified task associated with employees.
@@ -18,36 +18,20 @@ namespace TrainingTask.BLL.Interfaces
         void Update(CreateTask task);
 
         /// <summary>
-        /// Deletes the task by specified identifier.
-        /// </summary>
-        /// <param name="id">The identifier.</param>
-        void Delete(int id);
-
-        /// <summary>
-        /// Gets the task by specified identifier.
-        /// </summary>
-        /// <param name="id">The identifier.</param>
-        /// <returns></returns>
-        Task Get(int id);
-
-        /// <summary>
         /// Gets all tasks include employees.
         /// </summary>
-        /// <returns></returns>
         IEnumerable<TaskViewModel> GetAll();
 
         /// <summary>
         /// Gets the task including employees by specified identifier.
         /// </summary>
         /// <param name="id">The identifier.</param>
-        /// <returns></returns>
         TaskViewModel GetViewModel(int id);
 
         /// <summary>
         /// Gets all tasks including employees by project identifier.
         /// </summary>
         /// <param name="id">The identifier.</param>
-        /// <returns></returns>
         IEnumerable<TaskViewModel> GetByProjectId(int id);
     }
 }
--- src/TrainingTask.BLL/Services/ProjectService.cs
+++ src/TrainingTask.BLL/Services/ProjectService.cs
@@ -1,97 +1,32 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Transactions;
 using TrainingTask.BLL.Interfaces;
 using TrainingTask.Common.Models;
 using TrainingTask.DAL.Interfaces;
 
 namespace TrainingTask.BLL.Services
 {
-    public class ProjectService : IProjectService
+    public class ProjectService : Service<Project>, IProjectService
     {
-        private readonly IProjectRepository _projectRepository;
-        private readonly ITaskService _taskService;
+        private readonly IProjectRepository _repository;
 
-        public ProjectService(IProjectRepository projectRepository, ITaskService taskService)
+        public ProjectService(IProjectRepository repository) : base(repository)
         {
-            _projectRepository = projectRepository;
-            _taskService = taskService;
+            _repository = repository;
         }
 
         public int Add(CreateProject project)
         {
-            var nProject = new Project
-            {
-                Name = project.Name,
-                Abbreviation = project.Abbreviation,
-                Description = project.Description
-            };
-
-            using (var transactionScope = new TransactionScope(TransactionScopeOption.Required,
-                new TransactionOptions { IsolationLevel = IsolationLevel.Serializable }))
-            {
-                var insertedId = _projectRepository.Create(nProject);
-
-                if (project.Tasks != null)
-                {
-                    foreach (var projectTask in project.Tasks)
-                    {
-                        projectTask.ProjectId = insertedId;
-                        _taskService.Add(projectTask);
-                    }
-                }
-
-                transactionScope.Complete();
-                return insertedId;
-            }
-           }
-
-        public void Update(CreateProject modifiedProject)
-        {
-            var tasks = _taskService.GetByProjectId(modifiedProject.Id).Where(m => modifiedProject.Tasks.All(t => t.Id != m.Id)).ToArray();
-            var project = _projectRepository.Get(modifiedProject.Id);
-            project.Name = modifiedProject.Name;
-            project.Abbreviation = modifiedProject.Abbreviation;
-            project.Description = modifiedProject.Description;
-
-            using (var transactionScope = new TransactionScope(TransactionScopeOption.Required,
-                new TransactionOptions { IsolationLevel = IsolationLevel.Serializable }))
-            {
-                _projectRepository.Update(project);
-                foreach (var projectTask in modifiedProject.Tasks)
-                {
-                    projectTask.ProjectId = modifiedProject.Id;
-                    if (projectTask.Id == 0)
-                    {
-                        _taskService.Add(projectTask);
-                    }
-                    else
-                    {
-                        _taskService.Update(projectTask);
-                    }
-                }
-                foreach (var task in tasks)
-                {
-                    _taskService.Delete(task.Id);
-                }
-
-                transactionScope.Complete();
-            }
+            return _repository.Create(project);
         }
 
-        public void Delete(int id)
+        public void Update(CreateProject project)
         {
-            _projectRepository.Delete(id);
-        }
-
-        public Project Get(int id)
-        {
-            return _projectRepository.Get(id);
+            _repository.Update(project);
         }
 
         public IEnumerable<Project> GetAll()
         {
-            return _projectRepository.GetAll();
+            return _repository.GetAll();
         }
     }
 }
