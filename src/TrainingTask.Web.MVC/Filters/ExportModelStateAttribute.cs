27f1ecc6376fc9afbb9325ea85152bc077c546f3
krasovskayavs <krasovskayavs@QWS-VKS-7>
2019-01-11 14:48:25 +0300

    added MVC project, Employee controller, Views for employee controller

---
+++ src/TrainingTask.Web.MVC/Filters/ExportModelStateAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using TrainingTask.Web.MVC.Models.ModelState;
+
+namespace TrainingTask.Web.MVC.Filters
+{
+    public class ExportModelStateAttribute : ModelStateTransfer
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (!filterContext.ModelState.IsValid)
+            {
+                if (filterContext.Result is RedirectResult || filterContext.Result is RedirectToRouteResult || filterContext.Result is RedirectToActionResult)
+                {
+                    if (filterContext.Controller is Controller controller && filterContext.ModelState != null)
+                    {
+                        var modelState = ModelStateHelpers.SerializeModelState(filterContext.ModelState);
+                        controller.TempData[Key] = modelState;
+                    }
+                }
+            }
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
---
+++ src/TrainingTask.Web.MVC/Filters/ImportModelStateAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TrainingTask.Web.MVC.Models.ModelState;
+
+namespace TrainingTask.Web.MVC.Filters
+{
+    public class ImportModelStateAttribute : ModelStateTransfer
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var controller = filterContext.Controller as Controller;
+
+            if (controller?.TempData[Key] is string serializedModelState)
+            {
+                if (filterContext.Result is ViewResult)
+                {
+                    var modelState = ModelStateHelpers.DeserializeModelState(serializedModelState);
+                    filterContext.ModelState.Merge(modelState);
+                }
+                else
+                {
+                    controller.TempData.Remove(Key);
+                }
+            }
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
