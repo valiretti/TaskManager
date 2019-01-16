8994a81c87099ce87c3135bb25a9295a6a644637
krasovskayavs <krasovskayavs@QWS-VKS-7>
2019-01-16 15:51:17 +0300

    added enum status for view models

---
+++ src/TrainingTask.Web.MVC/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainingTask.Web.MVC.Models
+{
+    public enum Status
+    {
+        [Display(Name = "Not Started")]
+        NotStarted = 0,
+        [Display(Name = "In Progress")]
+        InProgress = 1,
+        Completed = 2,
+        Postponed = 3
+    }
+}
--- src/TrainingTask.Web.MVC/Models/TaskCreationModel.cs
+++ src/TrainingTask.Web.MVC/Models/TaskCreationModel.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using TrainingTask.Common.Enums;
 
 namespace TrainingTask.Web.MVC.Models
 {
