6ad10136f64d6ec4bbeed6101ced92baacbb967a
krasovskayavs <krasovskayavs@QWS-VKS-7>
2019-01-16 14:41:10 +0300

    added scripts for handling project

---
+++ src/TrainingTask.Web.MVC/wwwroot/js/common.js
@@ -0,0 +1,20 @@
+﻿function setTaskData(task) {
+    sessionStorage.setItem('task', JSON.stringify(task));
+}
+
+function addItem(taskForInsert) {
+    let taskString = sessionStorage.getItem('task');
+    let task = taskString ? JSON.parse(taskString) : [];
+    task.push(taskForInsert);
+    setTaskData(task);
+}
+
+function getFromStorage() {
+    let taskString = sessionStorage.getItem('task');
+    let tasks = taskString ? JSON.parse(taskString) : [];
+    return tasks;
+}
+
+function GetTask(tasks, id) {
+    return tasks.find(t => t.id == id || t.tempId == id);
+}
